f387869d4222973bf81c6c6c51d9ac4ca30344cb
Ciro-k <kirill00w@gmail.com>
2026-07-14 11:16:07 +0200

    Update LoadLibraryA.asm

--- LoadLibraryA.asm
+++ LoadLibraryA.asm
@@ -5,26 +5,26 @@
 
   xor r11, r11
 
-.search_loop_2:
+.search_loop_LoadLibrary:
   mov edi, dword ptr [r8 + r11 * 4
   add rdi, r15
 
   ;сверение r12
   mov rax, qword ptr [rdi]
   cmp rax, r12
-  jne. not_found_2
+  jne. not_found_LoadLibrary:
 
   ;сверение r13
   mov rax, qword ptr [rdi + 8]
   cmp rax, r13
-  je .function_found_2:
+  je .function_found_LoadLibrary:
 
-.not_found_2:
+.not_found_LoadLlibrary:
   inc r11
-  jmp .search_loop_2
+  jmp .search_loop_LoadLibrary:
 .function_found_2:
 
-.function_found_2
+.function_found_LoadLibrary:
   movzx r12d, word ptr [r9 + r11 * 2]
   mov eax, dword ptr [r10 + r12 * 4]
   add rax, r15
